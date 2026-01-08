73dc063b7ab374773c4f7e2a38c7c72f92f5fc5c
Abdul Zia <52299063+abdulziaa@users.noreply.github.com>
2026-01-07 23:56:51 -0700

    initial UART manager

---
+++ comm/uart/src/uart_manager.c
@@ -0,0 +1,50 @@
+#include "uart_manager.h"
+
+static const char *TAG = "UART MANAGER";
+
+// Static array to track the initialization of UART ports
+static bool uart_initialized[3] = {false};
+
+esp_err_t uart_manager_init(uart_port_t port, int rx, int tx, uint32_t baud, uart_parity_t parity, uart_stop_bits_t stop_bits, uart_hw_flowcontrol_t flow_control, uart_mode_t mode) 
+{
+    esp_err_t ret;
+
+    uart_config_t uart_config = {
+        .baud_rate = baud,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = parity,
+        .stop_bits = stop_bits,
+        .flow_ctrl = flow_control,
+        .rx_flow_ctrl_thresh = 122,
+    };
+
+    ret = uart_param_config(port, &uart_config);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to set uart port parameters!");
+        return ret;
+    }
+
+    ret = uart_set_pin(port, tx, rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to set uart communication pins!");
+        return ret;
+    }
+
+    return ESP_OK;
+};
+
+esp_err_t uart_flight_init(void)
+{   
+    esp_err_t ret;
+
+    ret = uart_manager_init(UART_NUM_0,44,43,115200,UART_PARITY_DISABLE,UART_STOP_BITS_1,UART_HW_FLOWCTRL_DISABLE, UART_MODE_UART);
+
+    if (ret != ESP_OK){
+        ESP_LOGE(TAG, "Failed to initialize UART manager!");
+        return ret;
+    }
+
+    ESP_LOGI(TAG, "SUCCESSFULLY INITIALIZED ALL UART BUSSES");
+
+    return ret;
+}
---
+++ comm/uart/src/uart_manager.h
@@ -0,0 +1,15 @@
+#ifndef UART_MANAGER_H
+#define UART_MANAGER_H
+
+#include "stdbool.h"
+#include "stdint.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "driver/uart.h"
+#include "ascent_r3_hardware_definition.h"
+
+esp_err_t uart_manager_init(uart_port_t port, int rx, int tx, uint32_t baud, uart_parity_t parity, uart_stop_bits_t stop_bits, uart_hw_flowcontrol_t flow_control, uart_mode_t mode);
+
+esp_err_t uart_flight_init(void);
+
+#endif
