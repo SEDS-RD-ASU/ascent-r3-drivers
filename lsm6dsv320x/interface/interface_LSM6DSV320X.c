03cf83f611b4487289de5b93162f06b8507e2b78
Abdul Zia <abdulzia@asu.edu>
2026-02-02 13:03:27 -0700

    scaling and transform added to lsm_get_local in IMU interface

--- lsm6dsv320x/interface/interface_LSM6DSV320X.c
+++ lsm6dsv320x/interface/interface_LSM6DSV320X.c
@@ -31,75 +31,76 @@ esp_err_t lsm_flight_init(spi_host_device_t host)
 
     ret = lsm_set_gyr_scale(LSM6DSV320X_4000dps);
     if(ret) return ret;
-
-    lsm_raw_data_t fakeRaw;
-    lsm_raw_data_t fakeCalibrated;
-    while(1)
-    {
-        lsm_get_raw(&fakeRaw);
-        fakeCalibrated = fakeRaw;
-        lsm_data_transform(&fakeCalibrated, &fakeRaw);
-        // lsm_data_scale(&fake);
-        // printf("Xg: %f, Yg: %f, Zg: %f\n", fakeRaw.gyr_x, fakeRaw.gyr_y, fakeRaw.gyr_z);
-        // printf("Xla: %f, Yla: %f, Zla: %f\n", fakeCalibrated.lowacc_x, fakeCalibrated.lowacc_y, fakeCalibrated.lowacc_z);
-        // printf("Xha: %f, Yha: %f, Zha: %f\n", fakeRaw.highacc_x, fakeRaw.highacc_y, fakeRaw.highacc_z);
-        printf("Xha: %f, Yha: %f, Zha: %f\n", fakeCalibrated.highacc_x, fakeCalibrated.highacc_y, fakeCalibrated.highacc_z);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
 }
 
-esp_err_t lsm_data_transform(lsm_raw_data_t* dataOutput, lsm_raw_data_t* dataInput){
-// Apply rotation matrix to gyro
-#define INV_SQRT2 0.7071067811865475f
+// Transform sensor x and y data s.t. y axis is oriented along long side, x axis is oriented along short side.
+// y
+// ^
+// |
+// |
+// |
+// 0 - - - - - > x
+static esp_err_t lsm_transform(lsm_raw_data_t* data){
+    
+    float transformed_gyr_x = (-0.707106f * data->gyr_x) - (0.707106f * data->gyr_y);
+    float transformed_gyr_y = (0.707106f * data->gyr_x) - (0.707106f * data->gyr_y);
 
-    if (dataOutput == NULL || dataInput == NULL) {
-        return ESP_ERR_INVALID_ARG;
-    }
+    float transformed_lowacc_x = (-0.707106f * data->lowacc_x) - (0.707106f * data->lowacc_y);
+    float transformed_lowacc_y = (0.707106f * data->lowacc_x) - (0.707106f * data->lowacc_y);
 
-    // // Gyro
-    // dataOutput->gyr_x =
-    //     (dataInput->gyr_x - dataInput->gyr_z) * INV_SQRT2;
+    float transformed_highacc_x = (-0.707106f * data->highacc_x) - (0.707106f * data->highacc_y);
+    float transformed_highacc_y = (0.707106f * data->highacc_x) - (0.707106f * data->highacc_y);
 
-    // dataOutput->gyr_y =
-    //     dataInput->gyr_y; 
+    data->gyr_x = transformed_gyr_x;
+    data->gyr_y = transformed_gyr_y;
 
-    // dataOutput->gyr_z =
-    //     (dataInput->gyr_x + dataInput->gyr_z) * INV_SQRT2;
+    data->lowacc_x = transformed_lowacc_x;
+    data->lowacc_y = transformed_lowacc_y;
 
-    // ---- Low-g accel ----
-    // dataOutput->lowacc_x = dataInput->lowacc_x;
-
-    // dataOutput->lowacc_y = ((dataInput->lowacc_y * 0.061f)/1000.0f) * 0.7071067811865475f;  
-
-    // dataOutput->lowacc_z = dataInput->lowacc_z;
-
-    // // High-G Accel
-    dataOutput->highacc_x =
-        (dataInput->highacc_x - dataInput->highacc_z) * INV_SQRT2;
-
-    dataOutput->highacc_y =
-        dataInput->highacc_y* INV_SQRT2;  
-    dataOutput->highacc_z =
-        (dataInput->highacc_x + dataInput->highacc_z) * INV_SQRT2;
+    data->highacc_x = transformed_highacc_x;
+    data->highacc_y = transformed_highacc_y;
 
     return ESP_OK;
 }
 
-esp_err_t lsm_data_scale(lsm_raw_data_t* data, lsm6dsv320x_gy_full_scale_t gyScale, 
-    lsm6dsv320x_xl_full_scale_t xlScale, lsm6dsv320x_hg_xl_full_scale_t scale){
-        data->gyr_x *= gyScale;
-        data->gyr_y *= gyScale;
-        data->gyr_z *= gyScale;
-        data->lowacc_x *= xlScale;
-        data->lowacc_y *= xlScale;
-        data->lowacc_z *= xlScale;
-    
-        data->highacc_x *= scale;
-        data->highacc_y *= scale;
-        data->highacc_z *= scale;
-    
-        return ESP_OK;
-    }
+// For 16g/320g & 4000dps mode only. This is a temporary patch that needs to be revisited.
+static esp_err_t lsm_scale(lsm_raw_data_t* data)
+{
+    float scaled_gyr_x = data->gyr_x * 0.14f;
+    float scaled_gyr_y = data->gyr_y * 0.14f;
+    float scaled_gyr_z = data->gyr_z * 0.14f;
 
+    float scaled_lowacc_x = data->lowacc_x * 0.00478728f;
+    float scaled_lowacc_y = data->lowacc_y * 0.00478728f;
+    float scaled_lowacc_z = data->lowacc_z * 0.00478728f;
     
+    float scaled_highacc_x = data->highacc_x * 0.10219077f;
+    float scaled_highacc_y = data->highacc_y * 0.10219077f;
+    float scaled_highacc_z = data->highacc_z * 0.10219077f;
+
+    data->gyr_x = scaled_gyr_x;
+    data->gyr_y = scaled_gyr_y;
+    data->gyr_z = scaled_gyr_z;
+
+    data->lowacc_x = scaled_lowacc_x;
+    data->lowacc_y = scaled_lowacc_y;
+    data->lowacc_z = scaled_lowacc_z;
+
+    data->highacc_x = scaled_highacc_x;
+    data->highacc_y = scaled_highacc_y;
+    data->highacc_z = scaled_highacc_z;
+
+    return ESP_OK;
+}
+
+esp_err_t lsm_get_local(lsm_raw_data_t* local)
+{
+    esp_err_t ret = lsm_get_raw(local);
+    if(ret) return ret;
+
+    lsm_transform(local);
+    lsm_scale(local);
+
+    return ESP_OK;
+}
     
