09f008ed4d37052b30f6ab4ff8e10ff029e6082e
Abdul Zia <abdulzia@asu.edu>
2026-02-02 13:35:37 -0700

    fixed bug -- no return code on lsm_flight_init

--- lsm6dsv320x/interface/interface_LSM6DSV320X.c
+++ lsm6dsv320x/interface/interface_LSM6DSV320X.c
@@ -1,6 +1,5 @@
 #include "interface_LSM6DSV320X.h"
 
-
 esp_err_t lsm_flight_init(spi_host_device_t host)
 {
     esp_err_t ret;
@@ -31,6 +30,8 @@ esp_err_t lsm_flight_init(spi_host_device_t host)
 
     ret = lsm_set_gyr_scale(LSM6DSV320X_4000dps);
     if(ret) return ret;
+
+    return ESP_OK;
 }
 
 // Transform sensor x and y data s.t. y axis is oriented along long side, x axis is oriented along short side.
@@ -46,10 +47,10 @@ static esp_err_t lsm_transform(lsm_raw_data_t* data){
     float transformed_gyr_y = (0.707106f * data->gyr_x) - (0.707106f * data->gyr_y);
 
     float transformed_lowacc_x = (-0.707106f * data->lowacc_x) - (0.707106f * data->lowacc_y);
-    float transformed_lowacc_y = (0.707106f * data->lowacc_x) - (0.707106f * data->lowacc_y);
+    float transformed_lowacc_y = -1*((0.707106f * data->lowacc_x) - (0.707106f * data->lowacc_y));
 
     float transformed_highacc_x = (-0.707106f * data->highacc_x) - (0.707106f * data->highacc_y);
-    float transformed_highacc_y = (0.707106f * data->highacc_x) - (0.707106f * data->highacc_y);
+    float transformed_highacc_y = -1*((0.707106f * data->highacc_x) - (0.707106f * data->highacc_y));
 
     data->gyr_x = transformed_gyr_x;
     data->gyr_y = transformed_gyr_y;
